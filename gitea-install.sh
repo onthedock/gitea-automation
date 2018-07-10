ebbc8a935559cf9d34de979a1110e297a7d96747
Xavi Aznar <xavi.aznar@outlook.com>
2018-07-10 13:02:24 -0700

    Crea el contenedor de base de datos.

--- gitea-install.sh
+++ gitea-install.sh
@@ -1,12 +1,15 @@
 
 DB=mysql 	# mysql, mariadb 
 DB_VERSION=5.7	# tag/version
+MYSQL_ROOT_PASSWORD=VucWftX5ITR7
+MYSQL_PASSWORD=GHh5R9AySsyT
 
 GITEA_VERSION=1.3.2
-GITEA_DATA_VOLUME=data-gitea
+GITEA_DATA_VOLUME=gitea-data
 
 GITEA_UI_PORT=3000
 GITEA_SSH_PORT=22
+
 
 if [ $GITEA_VERSION != "latest" ]
 then
@@ -17,3 +20,11 @@ fi
 echo "Downloading $DB docker image..."
 sudo docker pull $DB:$DB_VERSION
 
+echo "Creating data volume $GITEA_DATA_VOLUME ..."
+sudo docker volume create $GITEA_DATA_VOLUME
+
+echo "Creating a user-defined bridge..."
+sudo docker network create gitea-net
+
+echo "Launch $DB container..."
+sudo docker run -d --name gitea-db -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=gitea -e MYSQL_USER=gitea -e MYSQL_PASSWORD=$MYSQL_PASSWORD  --mount source=$GITEA_DATA_VOLUME,target=/var/lib/mysql --network gitea-net $DB:$DB_VERSION
