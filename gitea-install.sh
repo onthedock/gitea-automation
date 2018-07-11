660168fc115fd20dacb32490f69f65476293b632
Xavi Aznar <xavi.aznar@outlook.com>
2018-07-11 09:35:58 -0700

    Agregada la creacion del contenedor de Gitea.

--- gitea-install.sh
+++ gitea-install.sh
@@ -5,6 +5,8 @@ MYSQL_ROOT_PASSWORD=VucWftX5ITR7
 MYSQL_PASSWORD=GHh5R9AySsyT
 
 GITEA_VERSION=1.3.2
+
+GITEA_DB_VOLUME=gitea-db
 GITEA_DATA_VOLUME=gitea-data
 
 GITEA_UI_PORT=3000
@@ -23,8 +25,14 @@ sudo docker pull $DB:$DB_VERSION
 echo "Creating data volume $GITEA_DATA_VOLUME ..."
 sudo docker volume create $GITEA_DATA_VOLUME
 
+echo "Creating data volume for Gitea's DB..."
+sudo docker volume create $GITEA_DB_VOLUME
+
 echo "Creating a user-defined bridge..."
 sudo docker network create gitea-net
 
 echo "Launch $DB container..."
-sudo docker run -d --name gitea-db -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=gitea -e MYSQL_USER=gitea -e MYSQL_PASSWORD=$MYSQL_PASSWORD  --mount source=$GITEA_DATA_VOLUME,target=/var/lib/mysql --network gitea-net $DB:$DB_VERSION
+sudo docker run -d --name gitea-db -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -e MYSQL_DATABASE=gitea -e MYSQL_USER=gitea -e MYSQL_PASSWORD=$MYSQL_PASSWORD  --mount source=$GITEA_DB_VOLUME,target=/var/lib/mysql --network gitea-net $DB:$DB_VERSION
+
+echo "Launch Gitea $GITEA_VERSION container..."
+sudo docker run -d --name gitea  --network gitea-net -p 2200:22 -p 3000:3000 --mount source=$GITEA_DATA_VOLUME,target=/data gitea/gitea:$GITEA_VERSION
