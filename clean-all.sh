07dee31dfee80baf465c0f702a6def0b2112c204
Xavi Aznar <xavi.aznar@outlook.com>
2018-07-11 09:58:15 -0700

    Cambiado el orden para eliminar los volumenes y la red despues de eliminar los contenedores.

--- clean-all.sh
+++ clean-all.sh
@@ -1,7 +1,8 @@
-sudo docker volume rm gitea-data
-sudo docker volume rm gitea-db
-sudo docker network rm gitea-net
 sudo docker stop gitea-db
 sudo docker rm gitea-db
 sudo docker stop gitea
 sudo docker rm gitea
+sudo docker volume rm gitea-data
+sudo docker volume rm gitea-db
+sudo docker network rm gitea-net
+
